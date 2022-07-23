681a76e90cc13dcf63cda079a00aa19709bcc438
thirunavukarasu <thirunavukarasu27696@gmail.com>
2022-07-23 10:56:54 +0530

    options test passess

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,11 +1,11 @@
 import { rest } from "msw";
 
 export const handlers = [
-  rest.get("http://localhost:3030/scoops", (req, req, ctx) => {
+  rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
     return res(
       ctx.json([
-        { name: "Chocolate", imagePath: "/images/chocolate.png" },
-        { name: "Vanilla", imagePath: "/images/vanilla.png" },
+        { name: "Chocolate scoop", imagePath: "/images/chocolate.png" },
+        { name: "Vanilla scoop", imagePath: "/images/vanilla.png" },
       ])
     );
   }),
