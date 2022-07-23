fc31c5039cf5c93a5efa493845ed64cdfdcb61b8
thirunavukarasu <thirunavukarasu27696@gmail.com>
2022-07-23 11:57:46 +0530

    Test and code alert for server error response

---
+++ src/pages/common/AlertBanner.jsx
@@ -0,0 +1,11 @@
+import { Alert } from "react-bootstrap";
+export default function AlertBanner({ message, variant }) {
+  const alertMessage =
+    message || "An unexpected error ocurred, please try again later";
+  const alertVariant = variant || "danger";
+  return (
+    <Alert variant={alertVariant} style={{ backgroundColor: "red" }}>
+      {alertMessage}
+    </Alert>
+  );
+}
--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,19 +1,25 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Row } from "react-bootstrap";
+import AlertBanner from "../common/AlertBanner";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToopingOption";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {});
+      .catch((error) => {
+        setError(true);
+      });
   }, [optionType]);
-  console.log(items);
+  if (error) {
+    return <AlertBanner />;
+  }
   const ItemComponent =
     optionType === "scoops"
       ? ScoopOption
